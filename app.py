40828d3546733e9d6ebcb76903f58684f571bee9
Moh Jaddoa <91413082+mohjaddoa@users.noreply.github.com>
2024-06-27 19:45:12 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents import create_pandas_dataframe_agent
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Data App')
