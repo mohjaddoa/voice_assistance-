da010a612fe12631e9901e4f1a721f0a2567880f
Moh Jaddoa <91413082+mohjaddoa@users.noreply.github.com>
2024-06-28 22:02:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ from langchain.chains import RetrievalQA
 from langchain_community.chat_models import ChatOpenAI
 import pysqlite3
 import sys
-sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 ## title
 st.markdown("<h1 style='text-align: center;'>AI Voice Assistant</h1>", unsafe_allow_html=True)
 
