de18346fd0adc3952d577defa6d6c24da050660d
Moh Jaddoa <91413082+mohjaddoa@users.noreply.github.com>
2024-06-28 21:56:30 +1000

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,6 +9,9 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_community.chat_models import ChatOpenAI
+import(‘pysqlite3’)
+import sys
+sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
 os.environ["OPENAI_API_KEY"] = "sk-proj-GD0JQam05SC8m2yGbTrHT3BlbkFJr75PkmtTqnCGFEAbkQbp"
 
 '''get_paths function take upload_files as input from
